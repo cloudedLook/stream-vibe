0f18a8236f6b98f2f9e3809adbb9dfe1536dda2e
Alexander <cloudedlook@mail.ru>
2025-03-03 16:10:07 +0300

    project setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import "@/styles/example.scss"
+import { Head } from "minista"
+
+export default function () {
+  return (
+    <>
+      <Head>
+        <title>Hello!</title>
+      </Head>
+      <h1>Hello!</h1>
+    </>
+  )
+}
